863633acecb277ad41e5856321c2b15f04e8bf24
air <sameg14@gmail.com>
2016-04-28 21:27:44 -0500

    Added new routes

---
+++ src/FoodTruckBundle/Controller/DefaultController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace FoodTruckBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction()
+    {
+        return $this->render('FoodTruckBundle:Default:index.html.twig');
+    }
+    
+    public function jessicaAction(){
+
+        die('I am jessica');
+    }
+}
---
+++ src/FoodTruckBundle/FoodTruckBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace FoodTruckBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class FoodTruckBundle extends Bundle
+{
+}
